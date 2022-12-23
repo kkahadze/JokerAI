2f4ba2472c45aad420fefd7043204efdbe51db38
Konstantine Kahadze <konstantinekahadze@gmail.com>
2022-12-23 00:17:26 -0500

    Removed table.py

--- src/table.py
+++
@@ -1,9 +0,0 @@
-from src.card import Card
-from src.deck import Deck
-
-class Table:
-    def __init__(self, deck=Deck()):
-        self.deck = deck
-        self.players = []
-        self.deck.shuffle()
-        self.deal()
