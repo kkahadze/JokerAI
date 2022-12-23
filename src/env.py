ef06bb4842bb9c87aa5291d6fef72dd49a55ba9a
Konstantine Kahadze <konstantinekahadze@gmail.com>
2022-12-23 15:11:20 -0500

    Added table clearly showing actions

--- src/env.py
+++ src/env.py
@@ -42,22 +42,25 @@ class JokerEnv(gym.Env):
                         ),
                         
                     }
-                )
-
-                "player": spaces.Dict(
-                    {
-                        # the first one is 36 since the player must have at least one card in possesion when taking an action
-                        "hand": spaces.MultiDiscrete([36, 37, 37, 37, 37, 37, 37, 37, 37]), 
-                        "desired": spaces.Discrete(10), # 0-9
-                        "taken": spaces.Discrete(10), # 0-9
-                    }
                 ),
-                
-                # "gone": spaces.MultiBinary(36),
+                # "gone": spaces.MultiBinary(36)
             }
         )
 
         # Actions represent the card that a player chooses to play.
+        # SUIT: DIAMONDS    CLUBS   HEARTS    SPADES
+        # VALUES
+        # 6:        0         1       2         3
+        # 7:        4         5       6         7
+        # 8:        8         9       10        11
+        # 9:        12        13      14        15
+        # 10:       16        17      18        19
+        # JACK:     20        21      22        23
+        # QUEEN:    24        25      26        27
+        # KING:     28        29      30        31
+        # ACE:      32        33      34        35
         self.action_space = spaces.Discrete(36)
+
+        
 
 
