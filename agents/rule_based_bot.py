f91d2efba7dd05faec5edca62221e7abf173ac05
Konstantine Kahadze <konstantinekahadze@gmail.com>
2022-12-23 15:07:22 -0500

    state space accounts for other players

---
+++ agents/rule_based_bot.py
@@ -0,0 +1,6 @@
+
+class RuleBasedAgent():
+    def __init__(self, env):
+        self.env = env
+        
+    def act(self, observation):
--- src/joker_env.py
+++ src/joker_env.py
@@ -12,6 +12,38 @@ class JokerEnv(gym.Env):
                 # all can be any card or no card (meaning that the current player is the first to play)
                 "in_play": spaces.MultiDiscrete([37, 37, 37]), 
                 "wild_suit": spaces.Discrete(4),
+                "players": spaces.Dict(
+                    {
+                        "0": spaces.Dict(
+                            {
+                                # the first one is 36 since the player must have at least one card in possesion when taking an action
+                                "hand": spaces.MultiDiscrete([36, 37, 37, 37, 37, 37, 37, 37, 37]), 
+                                "desired": spaces.Discrete(10), # 0-9
+                                "taken": spaces.Discrete(10), # 0-9
+                            }        
+                        ),
+                        "1": spaces.Dict(
+                            {
+                                "desired": spaces.Discrete(10), # 0-9
+                                "taken": spaces.Discrete(10), # 0-9
+                            }        
+                        ),
+                        "2": spaces.Dict(
+                            {
+                                "desired": spaces.Discrete(10), # 0-9
+                                "taken": spaces.Discrete(10), # 0-9
+                            }        
+                        ),
+                        "3": spaces.Dict(
+                            {
+                                "desired": spaces.Discrete(10), # 0-9
+                                "taken": spaces.Discrete(10), # 0-9
+                            }        
+                        ),
+                        
+                    }
+                )
+
                 "player": spaces.Dict(
                     {
                         # the first one is 36 since the player must have at least one card in possesion when taking an action
@@ -20,7 +52,8 @@ class JokerEnv(gym.Env):
                         "taken": spaces.Discrete(10), # 0-9
                     }
                 ),
-                "gone": spaces.MultiBinary(36),
+                
+                # "gone": spaces.MultiBinary(36),
             }
         )
 
